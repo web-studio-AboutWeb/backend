3b1b6d1dbf8b95004d1278dee62c87c79bd0049b
Ilya Sokolov <is@incsecurity.kz>
2023-09-17 20:06:35 +0300

    Updated main.go file.

--- cmd/main.go
+++ cmd/main.go
@@ -2,28 +2,17 @@ package main
 
 import (
 	"flag"
-	"github.com/joho/godotenv"
 	"log"
+
 	"web-studio-backend/internal/app/app"
 )
 
 func main() {
-	readEnv := flag.String("env-file", "", "Path to application env file.")
-	configPath := flag.String("config-path", "config.yml", "Path to application config file.")
+	var configPath string
+	flag.StringVar(&configPath, "config-path", "config.default.yml", "Path to application config file.")
 	flag.Parse()
 
-	if *readEnv != "" {
-		if err := godotenv.Load(*readEnv); err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	application, err := app.New(*configPath)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err := application.Start(); err != nil {
+	if err := app.Run(configPath); err != nil {
 		log.Fatal(err)
 	}
 }
