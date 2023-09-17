2b60881939c04350f7d4526b46b513fe9f9e51a0
Ilya Sokolov <is@incsecurity.kz>
2023-09-17 20:03:29 +0300

    Added postgres package.

---
+++ pkg/postgres/postgres.go
@@ -0,0 +1,83 @@
+package postgres
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/url"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+const (
+	_defaultMaxPoolSize  = 1
+	_defaultConnAttempts = 10
+	_defaultConnTimeout  = time.Second
+)
+
+// Postgres -.
+type Postgres struct {
+	maxPoolSize  int
+	connAttempts int
+	connTimeout  time.Duration
+
+	Pool *pgxpool.Pool
+}
+
+// New -.
+func New(ctx context.Context, url string, opts ...Option) (*Postgres, error) {
+	pg := &Postgres{
+		maxPoolSize:  _defaultMaxPoolSize,
+		connAttempts: _defaultConnAttempts,
+		connTimeout:  _defaultConnTimeout,
+	}
+
+	// Custom options
+	for _, opt := range opts {
+		opt(pg)
+	}
+
+	poolConfig, err := pgxpool.ParseConfig(url)
+	if err != nil {
+		return nil, fmt.Errorf("postgres - NewPostgres - pgxpool.ParseConfig: %w", err)
+	}
+
+	poolConfig.MaxConns = int32(pg.maxPoolSize)
+
+	for pg.connAttempts > 0 {
+		pg.Pool, err = pgxpool.NewWithConfig(ctx, poolConfig)
+		if err == nil {
+			break
+		}
+
+		log.Printf("Postgres is trying to connect, attempts left: %d", pg.connAttempts)
+
+		time.Sleep(pg.connTimeout)
+
+		pg.connAttempts--
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("postgres - NewPostgres - connAttempts == 0: %w", err)
+	}
+
+	return pg, nil
+}
+
+// Close -.
+func (p *Postgres) Close() {
+	if p.Pool != nil {
+		p.Pool.Close()
+	}
+}
+
+func ConnectionString(username, password, hostname, database string) string {
+	return (&url.URL{
+		Scheme:   "postgres",
+		User:     url.UserPassword(username, password),
+		Host:     hostname,
+		Path:     database,
+		RawQuery: "sslmode=disable",
+	}).String()
+}
