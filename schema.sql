f00d2158daf683865f8780076ac554b3df922b31
Ilya Sokolov <is@incsecurity.kz>
2023-09-17 20:06:48 +0300

    Updated schema.

--- schema.sql
+++ schema.sql
@@ -1,42 +1,37 @@
-create table if not exists projects
+CREATE DATABASE ws WITH OWNER webstudio;
+
+CREATE TABLE IF NOT EXISTS projects
 (
-  id           smallserial primary key,
-  title        text        not null,
-  description  text        not null,
-  cover_id     text,
-  started_at   timestamptz not null,
-  ended_at     timestamptz,
-  link text
+    id          SMALLSERIAL PRIMARY KEY,
+    title       TEXT        NOT NULL,
+    description TEXT        NOT NULL,
+    cover_id    TEXT,
+    started_at  timestamptz NOT NULL,
+    ended_at    timestamptz,
+    link        TEXT
 );
 
-create table if not exists project_documents
+CREATE TABLE IF NOT EXISTS project_documents
 (
-  id         bigserial not null,
-  project_id smallint  not null references projects (id) on delete cascade
+    id         BIGSERIAL PRIMARY KEY,
+    project_id SMALLINT NOT NULL REFERENCES projects (id) ON DELETE CASCADE
 );
 
-create type user_role as enum ('global admin', 'admin', 'moderator', 'user');
-create type user_position as enum ('frontend', 'backend', 'teamlead', 'manager', 'marketer');
-
-create table if not exists users
+CREATE TABLE IF NOT EXISTS users
 (
-  id          smallserial   not null primary key,
-  name        text          not null,
-  surname     text          not null,
-  login       text          not null,
-  password    text          not null,
-  role        user_role     not null default 'user',
-  position    user_position not null,
-  created_at  timestamptz   not null default now(),
-  disabled_at timestamptz
+    id          SMALLSERIAL PRIMARY KEY,
+    name        TEXT        NOT NULL,
+    surname     TEXT        NOT NULL,
+    login       TEXT        NOT NULL,
+    password    TEXT        NOT NULL,
+    role        int2        NOT NULL,
+    position    int2        NOT NULL,
+    created_at  timestamptz NOT NULL DEFAULT now(),
+    disabled_at timestamptz
 );
 
-create table if not exists project_participants
+CREATE TABLE IF NOT EXISTS project_participants
 (
-  project_id smallint not null references projects (id) on delete cascade,
-  user_id    smallint not null references users (id) on delete cascade
+    project_id SMALLINT NOT NULL REFERENCES projects (id) ON DELETE CASCADE,
+    user_id    SMALLINT NOT NULL REFERENCES users (id) ON DELETE CASCADE
 );
-
-insert into users(name, surname, login, password, role, position)
-values ('Денис', 'Камчатов', 'dkamchatov', 'something', 'admin', 'frontend')
-on conflict do nothing;
