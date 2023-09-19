b45eaebfaffbc751029f6f189941ca6f14224298
juicyluv <juicyluv@mail.ru>
2023-09-19 16:06:13 +0300

    Added updated_at fields.

--- migrations/000004_projects_table.up.sql
+++ migrations/000004_projects_table.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE projects
     id          serial PRIMARY KEY,
     title       text NOT NULL,
     description text NOT NULL,
-    started_at  text NOT NULL DEFAULT now(),
+    created_at  text NOT NULL DEFAULT now(),
     updated_at  text NOT NULL DEFAULT now(),
     team_id     int4 REFERENCES teams (id),
     cover_id    text,
--- migrations/000005_project_participants_table.up.sql
+++ migrations/000005_project_participants_table.up.sql
@@ -1,7 +1,9 @@
 CREATE TABLE project_participants
 (
-    project_id int4 NOT NULL REFERENCES projects (id),
-    user_id    int4 NOT NULL REFERENCES users (id),
-    role       int2 NOT NULL,
-    position   int2 NOT NULL
+    project_id int4        NOT NULL REFERENCES projects (id),
+    user_id    int4        NOT NULL REFERENCES users (id),
+    role       int2        NOT NULL,
+    position   int2        NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT now(),
+    updated_at timestamptz NOT NULL DEFAULT now()
 );
