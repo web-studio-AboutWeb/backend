f7ed9931a0c19df7dd8b370700bc208db687bf86
juicyluv <juicyluv@mail.ru>
2023-09-19 16:05:56 +0300

    Added migrations to upload script.

--- cmd/scripts/upload.sh
+++ cmd/scripts/upload.sh
@@ -13,18 +13,26 @@ fi
 echo "Compiling application..."
 GOOS=linux GOARCH=amd64 go build -o app -v -ldflags="-w -s" cmd/app/main.go
 
+echo "Compiling migrate..."
+GOOS=linux GOARCH=amd64 go build -o migrate -v -ldflags="-w -s" cmd/migrate/migrate.go
+
 echo "Compiling apidocs..."
 swag init -o web/static/apidocs --ot json -q -g cmd/app/main.go
+
+if [ $2 == "migrate" ] && [ $3 != "" ]; then
+  echo "migrate $3 will be executed."
+  MIGRATE="&& chmod +x ./migrate && ./migrate $3"
+fi
 
 echo "Enter password several times if asked, that's ok."
 
 ssh -t "$TARGET" "sudo rm /opt/ws/app"
 
-scp -r ./web ./app ./cmd/scripts/update.sh "$TARGET:/opt/ws"
+scp -r ./web ./app ./migrate ./migrations ./cmd/scripts/update.sh "$TARGET:/opt/ws"
 
 ssh -t "$TARGET" "cd /opt/ws &&
     (sudo tmux kill-session -t about-web || pkill tmux);
     chmod +x ./update.sh && sudo ./update.sh &&
-    sudo tmux new-session -s about-web -d \"sudo /opt/ws/app -config-path /opt/ws/config.yml \""
+    sudo tmux new-session -s about-web -d \"sudo /opt/ws/app -config-path /opt/ws/config.yml \" $MIGRATE "
 
 rm ./app
