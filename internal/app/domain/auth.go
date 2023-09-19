4d009f0ecb4750567a4bf0ec2cd32454d5f0de0b
juicyluv <juicyluv@mail.ru>
2023-09-19 15:29:53 +0300

    Added AuthContext.

--- internal/app/domain/auth.go
+++ internal/app/domain/auth.go
@@ -1,6 +1,13 @@
 package domain
 
 type (
+	AuthContext struct {
+		UserID   int32
+		Username string
+		Email    string
+		Role     UserRole
+	}
+
 	SignInRequest struct {
 		Login    string `json:"login"`
 		Password string `json:"password"`
--- internal/pkg/auth/context.go
+++ internal/pkg/auth/context.go
@@ -1,20 +1,24 @@
 package auth
 
-import "context"
+import (
+	"context"
+
+	"web-studio-backend/internal/app/domain"
+)
 
 const userKey = "user_id"
 
-func NewContext(ctx context.Context, id int32) context.Context {
-	return context.WithValue(ctx, userKey, id)
+func NewContext(ctx context.Context, ac *domain.AuthContext) context.Context {
+	return context.WithValue(ctx, userKey, ac)
 }
 
-func FromContext(ctx context.Context) int32 {
-	id, ok := ctx.Value(userKey).(int32)
+func FromContext(ctx context.Context) *domain.AuthContext {
+	ac, ok := ctx.Value(userKey).(*domain.AuthContext)
 	if !ok {
 		// Panic here because this will break application.
 		// Use this function only in appropriate places.
 		panic("cannot find user_id in context")
 	}
 
-	return id
+	return ac
 }
