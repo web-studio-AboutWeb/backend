0673e3919bfc02a498f46e035d4f17311b93fba4
juicyluv <juicyluv@mail.ru>
2023-09-20 20:59:17 +0300

    Described document.

--- internal/app/domain/document.go
+++ internal/app/domain/document.go
@@ -3,9 +3,12 @@ package domain
 import "time"
 
 type Document struct {
-	ID        int32     `json:"id"`
-	Filename  string    `json:"filename"`
-	CreatedAt time.Time `json:"createdAt"`
-	MimeType  string    `json:"mimeType"`
-	Size      int32     `json:"size"` // Size in bytes
+	ID               int32     `json:"id"`
+	OriginalFilename string    `json:"originalFilename"`
+	FileID           string    `json:"-"`
+	UserID           int32     `json:"userID"`
+	CreatedAt        time.Time `json:"createdAt"`
+	MimeType         string    `json:"mimeType"`
+	Size             int32     `json:"size"` // Size in bytes
+	Content          []byte    `json:"-"`
 }
