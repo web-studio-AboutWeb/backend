3c271fab64842485756036a2a3a7337d4b889589
juicyluv <juicyluv@mail.ru>
2023-09-20 20:58:20 +0300

    Added file id and user id fields.

--- migrations/000006_documents_table.up.sql
+++ migrations/000006_documents_table.up.sql
@@ -2,6 +2,8 @@ CREATE TABLE documents
 (
     id         serial PRIMARY KEY,
     filename   text        NOT NULL,
+    file_id    text        NOT NULL,
+    user_id    int4        NOT NULL REFERENCES users (id),
     mime       text        NOT NULL,
     size       int4        NOT NULL,
     created_at timestamptz NOT NULL DEFAULT now()
