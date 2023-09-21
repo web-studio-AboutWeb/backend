ebb8e2014c665c7e8986f559ddd9709f569d5149
juicyluv <juicyluv@mail.ru>
2023-09-21 16:10:23 +0300

    Added some fields to team.

--- migrations/000002_teams_table.up.sql
+++ migrations/000002_teams_table.up.sql
@@ -2,7 +2,9 @@ CREATE TABLE teams
 (
     id          serial PRIMARY KEY NOT NULL,
     title       text               NOT NULL,
+    description text               NOT NULL,
+    image_id    text               NOT NULL,
     created_at  timestamptz        NOT NULL DEFAULT now(),
-    updated_at  timestamptz,
+    updated_at  timestamptz        NOT NULL DEFAULT now(),
     disabled_at timestamptz
 );
--- migrations/000003_team_members_table.up.sql
+++ migrations/000003_team_members_table.up.sql
@@ -1,7 +1,8 @@
 CREATE TABLE team_members
 (
-    user_id  int4 NOT NULL REFERENCES users (id),
-    team_id  int4 NOT NULL REFERENCES teams (id),
-    role     int2 NOT NULL,
-    position int2 NOT NULL
+    user_id    int4        NOT NULL REFERENCES users (id),
+    team_id    int4        NOT NULL REFERENCES teams (id),
+    role       int2        NOT NULL,
+    position   int2        NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT now()
 );
