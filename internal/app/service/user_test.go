3fa3a2733243de36f4be8ed879693d852f748e0d
juicyluv <juicyluv@mail.ru>
2023-09-23 20:29:18 +0300

    Added user service tests.

--- internal/app/service/user_test.go
+++ internal/app/service/user_test.go
@@ -2,8 +2,11 @@ package service_test
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"path/filepath"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 	"go.uber.org/mock/gomock"
@@ -13,7 +16,7 @@ import (
 	"web-studio-backend/internal/app/service/mocks"
 )
 
-func user(t *testing.T) (*service.UserService, *mocks.MockUserRepository) {
+func user(t *testing.T) (*service.UserService, *mocks.MockUserRepository, *mocks.MockFileRepository) {
 	t.Helper()
 
 	mockCtl := gomock.NewController(t)
@@ -23,13 +26,13 @@ func user(t *testing.T) (*service.UserService, *mocks.MockUserRepository) {
 	fileRepo := mocks.NewMockFileRepository(mockCtl)
 	userService := service.NewUserService(userRepo, fileRepo)
 
-	return userService, userRepo
+	return userService, userRepo, fileRepo
 }
 
 func TestUserService_CreateUser(t *testing.T) {
 	t.Parallel()
 
-	serv, repo := user(t)
+	serv, repo, _ := user(t)
 
 	ctx := context.Background()
 
@@ -143,6 +146,525 @@ func TestUserService_CreateUser(t *testing.T) {
 			}
 
 			require.Equal(t, tc.res, res)
+
+			if !tc.wantErr {
+				return
+			}
+
+			require.Error(t, err)
+			if tc.err != nil {
+				require.ErrorAs(t, err, tc.err)
+			}
+		})
+	}
+}
+
+func TestUserService_GetUser(t *testing.T) {
+	t.Parallel()
+
+	serv, repo, _ := user(t)
+
+	ctx := context.Background()
+
+	tempTime := time.Now()
+
+	type test struct {
+		name    string
+		res     *domain.User
+		err     error
+		wantErr bool
+	}
+
+	tests := []struct {
+		test
+		id   int32
+		mock func(id int32)
+	}{
+		{
+			test: test{
+				name: "should pass",
+				res: &domain.User{
+					ID:         1,
+					Name:       "name",
+					Surname:    "surname",
+					Username:   "login",
+					Email:      "email",
+					Role:       2,
+					IsTeamLead: false,
+					CreatedAt:  tempTime,
+					UpdatedAt:  tempTime,
+					DisabledAt: &tempTime,
+					ImageID:    "image_id",
+				},
+				wantErr: false,
+			},
+			id: 1,
+			mock: func(id int32) {
+				repo.EXPECT().GetUser(ctx, id).Return(&domain.User{
+					ID:         1,
+					Name:       "name",
+					Surname:    "surname",
+					Username:   "login",
+					Email:      "email",
+					Role:       2,
+					IsTeamLead: false,
+					CreatedAt:  tempTime,
+					UpdatedAt:  tempTime,
+					DisabledAt: &tempTime,
+					ImageID:    "image_id",
+				}, nil)
+			},
+		},
+		{
+			test: test{
+				name:    "error",
+				wantErr: true,
+			},
+			id: 1,
+			mock: func(id int32) {
+				repo.EXPECT().GetUser(ctx, id).Return(nil, errors.New("unknown"))
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			tc.mock(tc.id)
+
+			res, err := serv.GetUser(ctx, tc.id)
+			if !tc.wantErr {
+				require.NoError(t, err)
+			} else {
+				require.Error(t, err)
+			}
+
+			require.Equal(t, tc.res, res)
+
+			if !tc.wantErr {
+				return
+			}
+
+			require.Error(t, err)
+			if tc.err != nil {
+				require.ErrorAs(t, err, tc.err)
+			}
+		})
+	}
+}
+
+func TestUserService_GetUsers(t *testing.T) {
+	t.Parallel()
+
+	serv, repo, _ := user(t)
+
+	ctx := context.Background()
+
+	tempTime := time.Now()
+
+	type test struct {
+		name    string
+		res     []domain.User
+		err     error
+		wantErr bool
+	}
+
+	tests := []struct {
+		test
+		mock func()
+	}{
+		{
+			test: test{
+				name: "should pass",
+				res: []domain.User{
+					{
+						ID:         1,
+						Name:       "name1",
+						Surname:    "surname1",
+						Username:   "login1",
+						Email:      "email1",
+						Role:       1,
+						IsTeamLead: false,
+						CreatedAt:  tempTime,
+						UpdatedAt:  tempTime,
+						DisabledAt: &tempTime,
+						ImageID:    "image_id1",
+					},
+					{
+						ID:         2,
+						Name:       "name2",
+						Surname:    "surname2",
+						Username:   "login2",
+						Email:      "email2",
+						Role:       2,
+						IsTeamLead: true,
+						CreatedAt:  tempTime,
+						UpdatedAt:  tempTime,
+						DisabledAt: &tempTime,
+						ImageID:    "image_id2",
+					},
+				},
+			},
+			mock: func() {
+				repo.EXPECT().GetUsers(ctx).Return([]domain.User{
+					{
+						ID:         1,
+						Name:       "name1",
+						Surname:    "surname1",
+						Username:   "login1",
+						Email:      "email1",
+						Role:       1,
+						IsTeamLead: false,
+						CreatedAt:  tempTime,
+						UpdatedAt:  tempTime,
+						DisabledAt: &tempTime,
+						ImageID:    "image_id1",
+					},
+					{
+						ID:         2,
+						Name:       "name2",
+						Surname:    "surname2",
+						Username:   "login2",
+						Email:      "email2",
+						Role:       2,
+						IsTeamLead: true,
+						CreatedAt:  tempTime,
+						UpdatedAt:  tempTime,
+						DisabledAt: &tempTime,
+						ImageID:    "image_id2",
+					},
+				}, nil)
+			},
+		},
+		{
+			test: test{
+				name:    "error",
+				wantErr: true,
+			},
+			mock: func() {
+				repo.EXPECT().GetUsers(ctx).Return(nil, errors.New("unknown"))
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			tc.mock()
+
+			res, err := serv.GetUsers(ctx)
+			if !tc.wantErr {
+				require.NoError(t, err)
+			} else {
+				require.Error(t, err)
+			}
+
+			require.Equal(t, tc.res, res)
+
+			if !tc.wantErr {
+				return
+			}
+
+			require.Error(t, err)
+			if tc.err != nil {
+				require.ErrorAs(t, err, tc.err)
+			}
+		})
+	}
+}
+
+func TestUserService_UpdateUser(t *testing.T) {
+	t.Parallel()
+
+	serv, repo, _ := user(t)
+
+	ctx := context.Background()
+
+	type test struct {
+		name    string
+		res     *domain.User
+		err     error
+		wantErr bool
+	}
+
+	tests := []struct {
+		test
+		user *domain.User
+		mock func(user *domain.User)
+	}{
+		{
+			test: test{
+				name: "should pass",
+				res: &domain.User{
+					ID:      1,
+					Name:    "name",
+					Surname: "surname",
+					Role:    1,
+				},
+				err:     nil,
+				wantErr: false,
+			},
+			user: &domain.User{
+				ID:      1,
+				Name:    "name",
+				Surname: "surname",
+				Role:    1,
+			},
+			mock: func(user *domain.User) {
+				repo.EXPECT().GetUser(ctx, user.ID).Return(&domain.User{
+					ID:      1,
+					Name:    "name",
+					Surname: "surname",
+					Role:    1,
+				}, nil)
+				repo.EXPECT().UpdateUser(ctx, user).Return(nil)
+				repo.EXPECT().GetUser(ctx, user.ID).Return(&domain.User{
+					ID:      1,
+					Name:    "name",
+					Surname: "surname",
+					Role:    1,
+				}, nil)
+			},
+		},
+		{
+			test: test{
+				name:    "update user error",
+				res:     nil,
+				wantErr: true,
+			},
+			user: &domain.User{},
+			mock: func(user *domain.User) {
+				repo.EXPECT().GetUser(ctx, user.ID).Return(&domain.User{
+					ID:      1,
+					Name:    "name",
+					Surname: "surname",
+					Role:    1,
+				}, nil)
+				repo.EXPECT().UpdateUser(ctx, user).Return(errors.New("update user error"))
+			},
+		},
+		{
+			test: test{
+				name:    "get user error",
+				res:     nil,
+				err:     nil,
+				wantErr: true,
+			},
+			user: &domain.User{},
+			mock: func(user *domain.User) {
+				repo.EXPECT().GetUser(ctx, user.ID).Return(nil, errors.New("get user error"))
+			},
+		},
+		{
+			test: test{
+				name:    "get updated user error",
+				res:     nil,
+				err:     nil,
+				wantErr: true,
+			},
+			user: &domain.User{},
+			mock: func(user *domain.User) {
+				repo.EXPECT().GetUser(ctx, user.ID).Return(&domain.User{
+					ID:      1,
+					Name:    "name",
+					Surname: "surname",
+					Role:    1,
+				}, nil)
+				repo.EXPECT().UpdateUser(ctx, user).Return(nil)
+				repo.EXPECT().GetUser(ctx, user.ID).Return(nil, errors.New("get user error"))
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			tc.mock(tc.user)
+
+			res, err := serv.UpdateUser(ctx, tc.user)
+			if !tc.wantErr {
+				require.NoError(t, err)
+			} else {
+				require.Error(t, err)
+			}
+
+			require.Equal(t, tc.res, res)
+
+			if !tc.wantErr {
+				return
+			}
+
+			require.Error(t, err)
+			if tc.err != nil {
+				require.ErrorAs(t, err, tc.err)
+			}
+		})
+	}
+}
+
+func TestUserService_GetUserImage(t *testing.T) {
+	t.Parallel()
+
+	serv, repo, fileRepo := user(t)
+
+	ctx := context.Background()
+
+	type test struct {
+		name    string
+		res     *domain.User
+		err     error
+		wantErr bool
+	}
+
+	tests := []struct {
+		test
+		id   int32
+		mock func(id int32)
+	}{
+		{
+			test: test{
+				name: "should pass",
+				res: &domain.User{
+					ID:           1,
+					ImageID:      "image_id",
+					ImageContent: []byte("image_content"),
+				},
+				err:     nil,
+				wantErr: false,
+			},
+			id: 1,
+			mock: func(id int32) {
+				repo.EXPECT().GetUser(ctx, id).Return(&domain.User{
+					ID:      1,
+					ImageID: "image_id",
+				}, nil)
+				fileRepo.EXPECT().Read(ctx, filepath.Join("users", "image_id")).Return([]byte("image_content"), nil)
+			},
+		},
+		{
+			test: test{
+				name:    "get user error",
+				res:     nil,
+				err:     nil,
+				wantErr: true,
+			},
+			mock: func(id int32) {
+				repo.EXPECT().GetUser(ctx, id).Return(nil, errors.New("get user error"))
+			},
+		},
+		{
+			test: test{
+				name:    "not found",
+				res:     nil,
+				wantErr: true,
+			},
+			mock: func(id int32) {
+				repo.EXPECT().GetUser(ctx, id).Return(&domain.User{
+					ID:      id,
+					ImageID: "",
+				}, nil)
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			tc.mock(tc.id)
+
+			res, err := serv.GetUserImage(ctx, tc.id)
+			if !tc.wantErr {
+				require.NoError(t, err)
+			} else {
+				require.Error(t, err)
+			}
+
+			require.Equal(t, tc.res, res)
+
+			if !tc.wantErr {
+				return
+			}
+
+			require.Error(t, err)
+			if tc.err != nil {
+				require.ErrorAs(t, err, tc.err)
+			}
+		})
+	}
+}
+
+func TestUserService_RemoveUser(t *testing.T) {
+	t.Parallel()
+
+	serv, repo, _ := user(t)
+
+	ctx := context.Background()
+
+	type test struct {
+		name    string
+		err     error
+		wantErr bool
+	}
+
+	tests := []struct {
+		test
+		id   int32
+		mock func(id int32)
+	}{
+		{
+			test: test{
+				name:    "should pass",
+				err:     nil,
+				wantErr: false,
+			},
+			id: 1,
+			mock: func(id int32) {
+				repo.EXPECT().GetUser(ctx, id).Return(&domain.User{
+					ID: 1,
+				}, nil)
+				repo.EXPECT().DisableUser(ctx, id).Return(nil)
+			},
+		},
+		{
+			test: test{
+				name:    "get user error",
+				err:     nil,
+				wantErr: true,
+			},
+			mock: func(id int32) {
+				repo.EXPECT().GetUser(ctx, id).Return(nil, errors.New("get user error"))
+			},
+		},
+		{
+			test: test{
+				name:    "disable error",
+				wantErr: true,
+			},
+			mock: func(id int32) {
+				repo.EXPECT().GetUser(ctx, id).Return(&domain.User{
+					ID: id,
+				}, nil)
+				repo.EXPECT().DisableUser(ctx, id).Return(errors.New("some err"))
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			tc.mock(tc.id)
+
+			err := serv.RemoveUser(ctx, tc.id)
+			if !tc.wantErr {
+				require.NoError(t, err)
+			} else {
+				require.Error(t, err)
+			}
 
 			if !tc.wantErr {
 				return
