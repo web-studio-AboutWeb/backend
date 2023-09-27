8a50085cd9f578b4a610b3111b1d3e9dc64d3b78
juicyluv <juicyluv@mail.ru>
2023-09-27 22:48:13 +0300

    CORS fix.

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ run: docs
 	go build -o app cmd/app/main.go && ./app -config-path config.default.yml
 
 mcreate:
-	migrate create -ext sql -dir migrations -seq $(name) # $ name=<migration_name> make mcreate
+	migrate create -ext sql -dir migrations -seq $(name) # $ make mcreate <migration_name>
 
 migrate-up:
 	go run cmd/migrate/migrate.go up
--- internal/app/handler/http/middleware.go
+++ internal/app/handler/http/middleware.go
@@ -8,9 +8,8 @@ import (
 
 func corsMiddleware() func(next http.Handler) http.Handler {
 	return cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"https://*", "http://*"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token", "multipart/form-data"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
 		AllowCredentials: false,
 	})
